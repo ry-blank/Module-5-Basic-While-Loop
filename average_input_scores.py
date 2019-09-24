af295d275bc00715a7cb6130f7fa81723a92733f
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-09-23 19:02:18 -0500

    Add files via upload
    2nd Commit

--- average_input_scores.py
+++ average_input_scores.py
@@ -1,5 +1,10 @@
 def average(score_list):
-    pass
+    total_of_scores = 0
+    items_in_list = len(score_list)
+    for x in score_list:
+        total_of_scores += x
+    average = total_of_scores / items_in_list
+    return round(float(average), 2)
 
 
 if __name__ == '__main__':
